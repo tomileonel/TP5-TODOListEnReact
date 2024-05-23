3c1f22f2abefd31cd717cfe8b36a75ff72bbdd28
TOMÁS LEONEL DEGESE <47349708@ort>
2024-05-23 10:38:26 -0300

    23520242ndobloque

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,17 +1,33 @@
-const Todo = ({ setTodos, todos, text, created, completed }) => {
+const Todo = ({ setTodos, todos, todo }) => {
+  const Completar = () => {
+      // Deberiamos:
+      /**
+       * Agarrar el objecto del array todos
+       * Cambiarlo
+       * Actualizar el array llamando a la funcion setTodos
+       */
+      const todo = todos.find((item) => item.text === todo.text);
+      if (todo) {
+        todo.completed = new Date();
+      }
 
-    const Completar = (elemento ) => {
+      
 
-    };
-  
-    return (
-      <li onClick={Completar(this)} data-created={created} data-completed={completed || ''}>
-        <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{text}</span> - Creado: {new Date(created).toLocaleString()}
-        {completed && ` - Completado: ${new Date(completed).toLocaleString()}`}
-      </li>
-    );
   };
-  
-  export default Todo;
-  
-  
+
+  return (
+    <li
+      onClick={Completar}
+      data-created={todo.created}
+      data-completed={todo.completed || ""}
+    >
+      <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+        {todo.text}
+      </span>{" "}
+      - Creado: {new Date(todo.created).toLocaleString()}
+      {todo.completed && ` - Completado: ${new Date(todo.completed).toLocaleString()}`}
+    </li>
+  );
+};
+
+export default Todo;
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,13 +1,16 @@
-
-    import Todo from '../Todo/Todo'
+import Todo from "../Todo/Todo";
 const Todos = ({ todos, setTodos }) => {
-    return (
-        <div className="citas">
+  return (
+    <div className="citas">
       {todos.map((todo, index) => (
-    <Todo key={index} setTodos={setTodos} todos={todos} text={todo.text} completed={todo.completed} created={todo.created}/>
-))}
-      </div>
-    )
-}
-export default Todos
-
+        <Todo
+          key={index}
+          setTodos={setTodos}
+          todos={todos}
+          todo={todo}
+        />
+      ))}
+    </div>
+  );
+};
+export default Todos;
